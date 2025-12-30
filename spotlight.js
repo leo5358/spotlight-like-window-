27518f0d67b7e878f5fafcb8857c00349010b8b7
leo5358 <leo1yang5358@gmail.com>
2025-12-30 20:56:52 +0800

    new feature: favicons

--- spotlight.js
+++ spotlight.js
@@ -15,7 +15,6 @@ function debounce(func, delay) {
   };
 }
 
-
 const handleInput = debounce(async () => {
   const query = input.value;
   selectedIndex = 0;
@@ -40,14 +39,12 @@ const handleInput = debounce(async () => {
 
 input.addEventListener("input", handleInput);
 
-
 function updateModeBadge(rawQuery) {
   const q = rawQuery.toLowerCase();
   
   modeBadge.className = "";
   modeBadge.textContent = "";
 
-  // [修改] 增加 Tab Mode 判斷
   if (q.startsWith("%t ")) {
     modeBadge.textContent = "Tab Mode";
     modeBadge.classList.add("show", "mode-tab");
@@ -63,12 +60,37 @@ function updateModeBadge(rawQuery) {
   }
 }
 
+function getFaviconSource(item) {
+  if (item.type === "search") {
+    return 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>';
+  }
+
+  if (item.url && item.url.startsWith("http")) {
+    try {
+      const urlObj = new URL(item.url);
+      return `https://www.google.com/s2/favicons?domain=${urlObj.hostname}&sz=64`;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  return 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gray"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>';
+}
+
 function renderList(list) {
   results.innerHTML = "";
   if (list.length === 0) return;
 
   list.forEach((item, i) => {
     const li = document.createElement("li");
+    
+    const img = document.createElement("img");
+    img.className = "favicon";
+    img.src = getFaviconSource(item);
+    
+    img.onerror = () => {
+      img.src = 'data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="gray"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-1 17.93c-3.95-.49-7-3.85-7-7.93 0-.62.08-1.21.21-1.79L9 15v1c0 1.1.9 2 2 2v1.93zm6.9-2.54c-.26-.81-1-1.39-1.9-1.39h-1v-3c0-.55-.45-1-1-1H8v-2h2c.55 0 1-.45 1-1V7h2c1.1 0 2-.9 2-2v-.41c2.93 1.19 5 4.06 5 7.41 0 2.08-.8 3.97-2.1 5.39z"/></svg>';
+    };
     
     const tag = document.createElement("span");
     tag.className = "tag";
@@ -80,6 +102,7 @@ function renderList(list) {
     const text = document.createElement("span");
     text.textContent = item.title;
     
+    li.appendChild(img);
     li.appendChild(tag);
     li.appendChild(text);
 
